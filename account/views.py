2f3caec0458d8efe7b302f6ba0ec056639a4a33b
dm1tro69 <dimolg22@gmail.com>
2020-03-14 23:07:55 +0200

    add profiles

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -27,4 +27,36 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         user_form = UserRegistrationForm(request.POST)
+#         if user_form.is_valid():
+#             # Создаем нового пользователя, но пока не сохраняем в базу данных.
+#             new_user = user_form.save(commit=False)
+#             # Задаем пользователю зашифрованный пароль.
+#             new_user.set_password(user_form.cleaned_data['password'])
+#             # Сохраняем пользователя в базе данных.
+#             new_user.save()
+#             return render(request, 'account/register_done.html', {'new_user': new_user})
+#     else:
+#         user_form = UserRegistrationForm()
+#     return render(request, 'account/register.html', {'user_form': user_form})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
 
